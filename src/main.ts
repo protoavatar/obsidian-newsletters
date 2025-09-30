b118abd88f551286a2fbee9d73101c8982e63573
protoavatar <protoavatar@outlook.com>
2025-09-30 11:37:48 -0300

    Fixed Casting to any

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { Plugin } from 'obsidian';
 import { NewslogSyncSettings } from './types';
 import { DEFAULT_SETTINGS, NewslogSettingTab } from './settings';
-import { registerCommands, downloadDailyBundle } from './commands';
+import {
+	registerCommands,
+	downloadDailyBundle as downloadDailyBundleCommand,
+} from './commands';
 import * as api from './api';
 
 export default class NewslogSyncPlugin extends Plugin {
 	settings!: NewslogSyncSettings;
 	api = api;
+	downloadDailyBundle = (date: string) => downloadDailyBundleCommand(this, date);
 
 	async onload() {
 		await this.loadSettings();
@@ -14,9 +18,6 @@ export default class NewslogSyncPlugin extends Plugin {
 		registerCommands(this);
 
 		this.addSettingTab(new NewslogSettingTab(this.app, this));
-
-		// Make the downloadDailyBundle function available globally on the plugin instance
-		(this as any).downloadDailyBundle = (date: string) => downloadDailyBundle(this, date);
 	}
 
 	onunload() {
